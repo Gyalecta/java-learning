36b2d5b49858a5e6f4afde00ae0776b93cc11c4a
Gyalecta <domenico.avino@proton.me>
2026-09-08 11:40:35 +0200

    feat: add hello world program

---
+++ 01-fundamentals/01-hello-world/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println(" \tJAVA ENGINEERING");
+        System.out.println(" \t     JOURNEY ");
+        System.out.println("==================================");
+
+        System.out.println(" Name: Domenico");
+        System.out.println(" Goal: Become a professional Java developer");
+        System.out.println(" Status: Learning");
+    }
+}
