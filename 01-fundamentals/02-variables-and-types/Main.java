e3cc4b3c7a86e05f8c0ce30888944858f60c4abf
Gyalecta <domenico.avino@proton.me>
2026-09-08 12:23:17 +0200

    Add: feat variables and types

---
+++ 01-fundamentals/02-variables-and-types/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Domenico";
+        int age = 20;
+        double height = 1.75;
+        boolean isStudent = true;
+        char initial = 'D';
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Initial: " + initial);
+
+        System.out.println("Age next year: " + (age + 1));
+
+        System.out.println("==================================");
+
+        int a = 10;
+        int b = a;
+
+        a = 20;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+     }
+ }
